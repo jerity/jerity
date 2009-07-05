ceb224fb2bb3724d49c50d20fba196844c27acda
Dave Ingram <dave@dmi.me.uk>
2009-07-05 17:59:11 +0000

    Added multi-level navigation and better "best url" algorithm

--- lib/ui/classes/NavigationMenu.php
+++ lib/ui/classes/NavigationMenu.php
@@ -5,11 +5,14 @@ class NavigationMenu implements Renderable {
   protected $bestMatchClass = null;
   protected $besturl = null;
   protected $urls = array();
+  protected $url_cache = array();
   protected $attrs = array();
   protected $ourUrl = '';
+  protected $levelHints = false;
 
   public function __construct($urls = array()) {
     $this->urls = $urls;
+    $this->refreshUrlCache();
   }
 
   public function setBestUrlClass($c) {
@@ -27,20 +30,39 @@ class NavigationMenu implements Renderable {
     return $_SERVER['REQUEST_URI'];
   }
 
-  protected function getBestUrl() {
-    if (!is_null($this->besturl)) {
-      return $this->besturl;
+  public function setLevelHints($hint) {
+    $this->levelHints = $hint;
+  }
+
+  protected function refreshUrlCache($urls = null, $level = 0) {
+    if (is_null($urls) && $level == 0) {
+      $this->url_cache = array();
     }
-    $this->besturl = false;
-    $cururl = $this->getOurUrl();
-    $matchlen = -1;
-    foreach ($this->urls as $u) {
-      if (strlen($u[1]) > $matchlen && substr($cururl, 0, strlen($u[1])) == $u[1]) {
-        $this->besturl = $u[1];
-        $matchlen = strlen($u[1]);
+    if (is_null($urls)) {
+      $urls = $this->urls;
+    }
+    foreach ($urls as $u) {
+      $this->url_cache[$level][] = $u[1];
+      if (isset($u[2]) && is_array($u[2]) && isset($u[2]['_children']) && is_array($u[2]['_children'])) {
+        $this->refreshUrlCache($u[2]['_children'], $level + 1);
       }
     }
-    return $this->besturl;
+  }
+
+  protected function getBestUrl($level = 0) {
+    if (isset($this->besturl[$level]) && !is_null($this->besturl[$level])) {
+      return $this->besturl[$level];
+    }
+    $this->besturl[$level] = false;
+    $cururl = $this->getOurUrl();
+    $matchlen = -1;
+    foreach ($this->url_cache[$level] as $u) {
+      if (strlen($u) <= strlen($cururl) && strlen($u) > $matchlen && preg_match('!^'.preg_quote(rtrim($u,'/')).'(/|$)!', $cururl)) {
+        $this->besturl[$level] = $u;
+        $matchlen = strlen($u);
+      }
+    }
+    return $this->besturl[$level];
   }
 
   protected static function renderTag($tag, array $attrs = null, $content = null, array $ignoreAttrs = null) {
@@ -70,11 +92,18 @@ class NavigationMenu implements Renderable {
     return $out;
   }
 
-  public function render() {
+  protected function renderURLs($urls, $top_attrs, $level=0) {
     $out = '';
-    $out .= self::renderTag('ul', $this->attrs)."\n";
-    $besturl = $this->getBestUrl();
-    foreach ($this->urls as $url) {
+    if ($this->levelHints) {
+      if (isset($top_attrs['class'])) {
+        $top_attrs['class'] .= ' level'.$level;
+      } else {
+        $top_attrs['class'] = 'level'.$level;
+      }
+    }
+    $out .= self::renderTag('ul', $top_attrs)."\n";
+    $besturl = $this->getBestUrl($level);
+    foreach ($urls as $url) {
       $a_text = htmlentities($url[0], ENT_QUOTES, 'UTF-8');
       $a_attrs = array('href'=>$url[1]);
       $i_attrs = isset($url[2]) ? $url[2] : array();
@@ -94,10 +123,20 @@ class NavigationMenu implements Renderable {
         $i_attrs['class'] = implode(' ', $i_class);
       }
 
-      $out .= self::renderTag('li', $i_attrs, self::renderTag('a', $a_attrs, $a_text))."\n";
+      $content = self::renderTag('a', $a_attrs, $a_text);
+      if (isset($i_attrs['_children']) && count($i_attrs['_children'])) {
+        $children = $i_attrs['_children'];
+        $child_attrs = isset($i_attrs['_child_attrs']) ? $i_attrs['_child_attrs'] : array();
+        $content .= "\n".$this->renderURLs($children, $child_attrs, $level + 1);
+      }
+      $out .= self::renderTag('li', $i_attrs, $content)."\n";
     }
     $out .= "</ul>\n";
     return $out;
+  }
+
+  public function render() {
+    return $this->renderURLs($this->urls, $this->attrs);
   }
 
   public function __toString() {
