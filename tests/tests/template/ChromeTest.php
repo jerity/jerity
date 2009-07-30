29ff315bfc51700ef7a62b2d2ecd6abc06e9578d
Dave Ingram <dave@dmi.me.uk>
2009-07-30 14:13:32 +0000

    Added Chrome title tests

--- tests/tests/template/ChromeTest.php
+++ tests/tests/template/ChromeTest.php
@@ -5,4 +5,34 @@ class ChromeTest extends PHPUnit_Framework_TestCase {
   public function setUp() {
     Template::setPath(dirname(dirname(__FILE__)).'/data/templates');
   }
+
+  public function titleSeparatorTest($sep) {
+    if ($sep === null) $sep = Chrome::getTitleSeparator();
+    $title = array('Jerity', 'test', 'title');
+    Chrome::setTitle($title);
+    $this->assertEquals(implode($sep, $title), Chrome::getTitle($sep));
+  }
+
+  public function testTitleSeparator() {
+    $tests = array(
+      null,
+      '',
+      ' ',
+      ' & ',
+      '&',
+      '&amp;',
+      ' &amp; ',
+      '&raquo;',
+      ' &raquo; ',
+    );
+    foreach ($tests as $test) {
+      $this->titleSeparatorTest($test);
+    }
+  }
+
+  public function testGetTitleArray() {
+    $title = array('Jerity', 'test', 'title');
+    Chrome::setTitle($title);
+    $this->assertEquals($title, Chrome::getTitle(false));
+  }
 }
