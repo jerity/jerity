689caeb76975c5860f69ed75d9a55d4182ad3b6a
Dave Ingram <dave@dmi.me.uk>
2009-07-30 14:13:32 +0000

    Added simple test chrome template and setUp() functions

--- tests/tests/template/ChromeTest.php
+++ tests/tests/template/ChromeTest.php
@@ -2,4 +2,7 @@
 require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/jerity.php');
 
 class ChromeTest extends PHPUnit_Framework_TestCase {
+  public function setUp() {
+    Template::setPath(dirname(dirname(__FILE__)).'/data/templates');
+  }
 }
