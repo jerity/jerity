2b8af4707befc6046b0de9ca435ba740bce0884b
Dave Ingram <dave@dmi.me.uk>
2009-07-31 11:06:27 +0000

    Added custom links test

--- tests/tests/template/ChromeTest.php
+++ tests/tests/template/ChromeTest.php
@@ -6,6 +6,32 @@ class ChromeTest extends PHPUnit_Framework_TestCase {
     Template::setPath(dirname(dirname(__FILE__)).'/data/templates');
   }
 
+  public function testCustomRelLink() {
+    Chrome::clearLinks();
+
+    Chrome::addLink('next', 'http://www.jerity.com/next');
+    $l = Chrome::getLinks();
+
+    $this->assertTrue(is_array($l));
+    $this->assertEquals(count($l), 1);
+    $this->assertEquals(count($l[0]), 2);
+    $this->assertEquals($l[0]['href'], 'http://www.jerity.com/next');
+    $this->assertEquals($l[0]['rel'],  'next');
+  }
+
+  public function testCustomRevLink() {
+    Chrome::clearLinks();
+
+    Chrome::addLink('author', 'mailto:info@jerity.com', true);
+    $l = Chrome::getLinks();
+
+    $this->assertTrue(is_array($l));
+    $this->assertEquals(count($l), 1);
+    $this->assertEquals(count($l[0]), 2);
+    $this->assertEquals($l[0]['rev'],  'author');
+    $this->assertEquals($l[0]['href'], 'mailto:info@jerity.com');
+  }
+
   public function titleSeparatorTest($sep) {
     if ($sep === null) $sep = Chrome::getTitleSeparator();
     $title = array('Jerity', 'test', 'title');
