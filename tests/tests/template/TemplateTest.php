b68a18b7a5b3753f0208ad321158fb17f96d8dbc
Dave Ingram <dave@dmi.me.uk>
2009-08-02 18:23:53 +0000

    Added basic Template tests

--- tests/tests/template/TemplateTest.php
+++ tests/tests/template/TemplateTest.php
@@ -1,8 +1,22 @@
 <?php
 require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/jerity.php');
 
+class TemplateT extends Template { }
+
 class TemplateTest extends PHPUnit_Framework_TestCase {
   public function setUp() {
     Template::setPath(dirname(dirname(__FILE__)).'/data/templates');
   }
+
+  public function testValidTemplate1() {
+    $t = new TemplateT('foo-succeed');
+  }
+
+  /**
+   * @expectedException RuntimeException
+   */
+  public function testInvalidTemplate() {
+    $t = new TemplateT('foo-fail');
+  }
+
 }
