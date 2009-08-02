5bc52b6b1f734c4e205ebb4cab23d81252fca904
Dave Ingram <dave@dmi.me.uk>
2009-08-02 18:24:21 +0000

    Added template jailbreaking tests

--- tests/tests/template/TemplateTest.php
+++ tests/tests/template/TemplateTest.php
@@ -19,4 +19,116 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     $t = new TemplateT('foo-fail');
   }
 
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak1() {
+    $t = new TemplateT('../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak2() {
+    $t = new TemplateT('./../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak3() {
+    $t = new TemplateT('../../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak4() {
+    $t = new TemplateT('./../../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak5() {
+    $t = new TemplateT('no-dir/../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak6() {
+    $t = new TemplateT('./no-dir/../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak7() {
+    $t = new TemplateT('no-dir/../../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak8() {
+    $t = new TemplateT('./no-dir/../../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak9() {
+    $t = new TemplateT('chrome/../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak10() {
+    $t = new TemplateT('./chrome/../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak11() {
+    $t = new TemplateT('chrome/../../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak12() {
+    $t = new TemplateT('./chrome/../../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak13() {
+    $t = new TemplateT('/foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak14() {
+    $t = new TemplateT('/abc/foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak15() {
+    $t = new TemplateT('/abc/../foo-fail');
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testJailbreak16() {
+    $t = new TemplateT('/abc/../../foo-fail');
+  }
+
 }
