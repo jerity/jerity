7f814dd939cfc70140b64e5af8e7e2000a551a3a
Dave Ingram <dave@dmi.me.uk>
2009-08-03 19:13:20 +0000

    Added organisation to Template tests
     - Added organisational comments
     - Added guard for defining test class

--- lib/template/tests/unit/TemplateTest.php
+++ lib/template/tests/unit/TemplateTest.php
@@ -1,12 +1,17 @@
 <?php
 require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/jerity.php');
 
-class TemplateT extends Template { }
+if (!class_exists('TemplateT')) {
+  class TemplateT extends Template { }
+}
 
 class TemplateTest extends PHPUnit_Framework_TestCase {
   public function setUp() {
     Template::setPath(dirname(dirname(__FILE__)).'/data/templates');
   }
+
+  ############################################################################
+  # Template validity tests {{{
 
   public function testValidTemplate1() {
     $t = new TemplateT('foo-succeed');
@@ -18,6 +23,12 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
   public function testInvalidTemplate() {
     $t = new TemplateT('foo-fail');
   }
+
+  # }}} Template validity tests
+  ############################################################################
+
+  ############################################################################
+  # Jailbreak tests {{{
 
   /**
    * @expectedException InvalidArgumentException
@@ -131,4 +142,9 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     $t = new TemplateT('/abc/../../foo-fail');
   }
 
+  # }}} Jailbreak tests
+  ############################################################################
+
 }
+
+# vim: ts=2 sw=2 et foldmethod=marker
