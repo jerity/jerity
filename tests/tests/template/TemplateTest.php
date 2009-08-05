747df456b53be0e9efd5ea8b1e8943fa549ac77c
Dave Ingram <dave@dmi.me.uk>
2009-08-05 21:20:14 +0100

    Added/updated a number of tests.
     - Updated template validity tests to include more negative tests
     - Added template validity tests to check template path
     - Updated template variable tests to use non-chrome template
     - Added "simple" template
     - Added test for clearing individual variables
     - Added negative template variable tests

--- tests/tests/template/TemplateTest.php
+++ tests/tests/template/TemplateTest.php
@@ -3,7 +3,9 @@ require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/jerity.php');
 
 if (!class_exists('TemplateT')) {
   // Template is an abstract class, so we need aa simple concrete implementation for testing
-  class TemplateT extends Template { }
+  class TemplateT extends Template {
+    public static function nullizePath() {parent::$base_path='';}
+  }
 }
 
 class TemplateTest extends PHPUnit_Framework_TestCase {
@@ -16,9 +18,56 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
   # Template validity tests {{{
 
   /**
+   * Test that an exception is thrown with an uninitialised template path.
+   *
+   * Note that this relies on a "cheating" method on TemplateT to reset the
+   * path that was set in the setUp() for the test.
+   *
+   * @expectedException  UnexpectedValueException
+   */
+  public function testNullPath() {
+    TemplateT::nullizePath();
+    try {
+      $path = Template::getPath();
+    } catch (InvalidArgumentException $e) {
+      return;
+    }
+    $this->fail();
+  }
+
+  /**
+   * Check a template path with a trailing slash.
+   */
+  public function testValidPath1() {
+    $path = dirname(dirname(__FILE__)).'/data/templates/';
+    Template::setPath($path);
+    $this->assertSame($path, Template::getPath());
+  }
+
+  /**
+   * Check a template path without a trailing slash is automagically given
+   * one.
+   */
+  public function testValidPath2() {
+    $path = dirname(dirname(__FILE__)).'/data/templates';
+    Template::setPath($path);
+    $this->assertNotSame($path, Template::getPath());
+    $this->assertSame("$path/", Template::getPath());
+  }
+
+  /**
+   * Fail if a non-existant template directory is given.
+   *
+   * @expectedException  InvalidArgumentException
+   */
+  public function testInvalidPath() {
+    Template::setPath(dirname(dirname(__FILE__)).'/data/no-templates');
+  }
+
+  /**
    * Ensure no errors when a valid template is loaded
    */
-  public function testValidTemplate1() {
+  public function testValidTemplate() {
     $t = new TemplateT('foo-succeed');
   }
 
@@ -31,6 +80,22 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
    */
   public function testInvalidTemplate() {
     $t = new TemplateT('foo-fail');
+  }
+
+  /**
+   * Fail if a template file doesn't exist at render time.
+   *
+   * @todo  This behaviour may change when automatic per-context templates are implemented
+   */
+  public function testInvalidRenderTemplate() {
+    touch(Template::getPath().'foo-fail.tpl.php');
+    $t = new TemplateT('foo-fail');
+    unlink(Template::getPath().'foo-fail.tpl.php');
+    try {
+      $t->render();
+      $this->fail();
+    } catch (RuntimeException $e) {
+    }
   }
 
   # }}} Template validity tests
@@ -85,7 +150,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
    * Simple assignment/retrieval test.
    */
   public function testSingleGetSet() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', 'bar');
     $this->assertSame('bar', $t->get('foo'));
   }
@@ -94,7 +159,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval test.
     */
   public function testMultipleGetSet1() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set(array('foo' => 'bar'));
     $this->assertSame('bar', $t->get('foo'));
   }
@@ -103,7 +168,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval test.
     */
   public function testMultipleGetSet2() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set(array('foo'), array('bar'));
     $this->assertSame('bar', $t->get('foo'));
   }
@@ -112,7 +177,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval test.
     */
   public function testMultipleGetSet3() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', 'bar');
     $t->set('baz', 'qux');
     $this->assertSame('bar', $t->get('foo'));
@@ -123,7 +188,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval test.
     */
   public function testMultipleGetSet4() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set(array('foo' => 'bar', 'baz' => 'qux'));
     $this->assertSame('bar', $t->get('foo'));
     $this->assertSame('qux', $t->get('baz'));
@@ -133,7 +198,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval test.
     */
   public function testMultipleGetSet5() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set(array('foo', 'baz'), array('bar', 'qux'));
     $this->assertSame('bar', $t->get('foo'));
     $this->assertSame('qux', $t->get('baz'));
@@ -143,7 +208,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval/clear test.
     */
   public function testGetSetSingleClearSingle() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', 'bar');
     $this->assertSame('bar', $t->get('foo'));
     $t->clear('foo');
@@ -154,7 +219,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval/clear test.
     */
   public function testGetSetSingleClearAll() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', 'bar');
     $this->assertSame('bar', $t->get('foo'));
     $t->clear();
@@ -165,7 +230,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     * Simple assignment/retrieval/clear test.
     */
   public function testGetSetMultipleClearIndividual() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', 'bar');
     $t->set('baz', 'qux');
     $this->assertSame('bar', $t->get('foo'));
@@ -181,8 +246,29 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
   /**
     * Simple assignment/retrieval/clear test.
     */
+  public function testGetSetMultipleClearGroup() {
+    $t = new TemplateT('simple');
+    $t->set('foo', 'bar');
+    $t->set('baz', 'qux');
+    $t->set('abc', 'def');
+    $this->assertSame('bar', $t->get('foo'));
+    $this->assertSame('qux', $t->get('baz'));
+    $this->assertSame('def', $t->get('abc'));
+    $t->clear(array('foo', 'baz'));
+    $this->assertSame(null, $t->get('foo'));
+    $this->assertSame(null, $t->get('baz'));
+    $this->assertSame('def', $t->get('abc'));
+    $t->clear();
+    $this->assertSame(null, $t->get('foo'));
+    $this->assertSame(null, $t->get('baz'));
+    $this->assertSame(null, $t->get('abc'));
+  }
+
+  /**
+    * Simple assignment/retrieval/clear test.
+    */
   public function testGetSetMultipleClearAll() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', 'bar');
     $t->set('baz', 'qux');
     $this->assertSame('bar', $t->get('foo'));
@@ -190,13 +276,15 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     $t->clear();
     $this->assertSame(null, $t->get('foo'));
     $this->assertSame(null, $t->get('baz'));
+    $vars = $t->get();
+    $this->assertEquals(0, count($vars));
   }
 
   /**
-    * Assignment/retrieval/clear test with non-trivial values.
+    * Assignment/retrieval test with non-trivial values.
     */
   public function testComplexSingleGetSet1() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $t->set('foo', array('bar'));
     $t->set('bar', array('baz', 'qux'));
     $this->assertSame(array('bar'), $t->get('foo'));
@@ -204,10 +292,10 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
   }
 
   /**
-    * Assignment/retrieval/clear test with non-trivial values.
+    * Assignment/retrieval test with non-trivial values.
     */
   public function testComplexSingleGetSet2() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $obj = new StdClass();
     $obj->foo = 0xba2;
     $obj->bar = 'quux';
@@ -217,12 +305,12 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
   }
 
   /**
-    * Assignment/retrieval/clear test with non-trivial values.
+    * Assignment/retrieval test with non-trivial values.
     *
     * NOTE: checks that objects are set by reference
     */
   public function testComplexSingleGetSet3() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $obj = new StdClass();
     $obj->foo = 0xba2;
     $obj->bar = 'quux';
@@ -233,12 +321,12 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
   }
 
   /**
-    * Assignment/retrieval/clear test with non-trivial values.
+    * Assignment/retrieval test with non-trivial values.
     *
     * NOTE: checks that objects are set by reference, and breaking that reference
     */
   public function testComplexSingleGetSet4() {
-    $t = new TemplateT('chrome/simple');
+    $t = new TemplateT('simple');
     $obj = new StdClass();
     $obj->foo = 0xba2;
     $obj->bar = 'quux';
@@ -247,6 +335,81 @@ class TemplateTest extends PHPUnit_Framework_TestCase {
     $obj->abc = 'def';
     $this->assertNotSame($obj, $t->get('test'));
     $this->assertNotEquals($obj, $t->get('test'));
+  }
+
+  /**
+   * @expectedException  LengthException
+   */
+  public function testSetFail1() {
+    $t = new TemplateT('simple');
+    $t->set(array('foo', 'bar'), array('baz'));
+  }
+
+  /**
+   * @expectedException  InvalidArgumentException
+   */
+  public function testSetFail2() {
+    $t = new TemplateT('simple');
+    $t->set(array('foo', 'bar'), 'baz');
+  }
+
+  public function testSetFail3() {
+    $t = new TemplateT('simple');
+    try {
+      $t->set(false, 'foo');
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+    try {
+      $t->set(new stdClass(), 'foo');
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+    try {
+      $t->set(0, 'foo');
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+  }
+
+  public function testClearFail() {
+    $t = new TemplateT('simple');
+    $t->set('foo', 'bar');
+    try {
+      $t->clear(false);
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+    try {
+      $t->clear(new stdClass());
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+    try {
+      $t->clear(0);
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+  }
+
+  public function testVarPrefixFail() {
+    $t = new TemplateT('simple');
+    $t->set('foo', 'bar');
+    try {
+      $t->setVariablePrefix(false);
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+    try {
+      $t->setVariablePrefix(new stdClass());
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
+    try {
+      $t->setVariablePrefix(0);
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+    }
   }
 
   # }}} Template variable tests
