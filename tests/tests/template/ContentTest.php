6c9a9f50697ab14b367e7d6fb278260e597a64f8
Dave Ingram <dave@dmi.me.uk>
2009-08-06 19:00:12 +0100

    Added complete testing for Content class

--- tests/tests/template/ContentTest.php
+++ tests/tests/template/ContentTest.php
@@ -5,4 +5,13 @@ class ContentTest extends PHPUnit_Framework_TestCase {
   public function setUp() {
     Template::setPath(DATA_DIR.'templates');
   }
+
+  public function testRender() {
+    $c = new Content('simple');
+    $c->set('content', '');
+    $this->assertSame('', $c->render());
+    $c->set('content', 'PASS');
+    $this->assertSame('PASS', $c->render());
+  }
+
 }
