18e9c3d388688b01100c9a8c58617f98b9ca827c
Nick Pope <nick@nickpope.me.uk>
2009-08-09 18:43:48 +0100

    Added support for choice of method in FormHelper

--- lib/form/classes/FormHelper.php
+++ lib/form/classes/FormHelper.php
@@ -14,6 +14,16 @@
  */
 class FormHelper {
 
+  const METHOD_POST = '_POST';
+  const METHOD_GET  = '_GET';
+
+  /**
+   * Controls which form-related superglobal(s) we should use when checking.
+   *
+   * @var  integer
+   */
+  protected static $method = self::METHOD_POST;
+
   /**
    * Non-instantiable class.
    */
@@ -21,15 +31,45 @@ class FormHelper {
   }
 
   /**
-   * Checks whether a POST item is empty.
+   * Gets the superglobal(s) we should use when checking.
    *
-   * @param  string  $field  The POST field to check.
+   * @return  integer
+   */
+  public static function getMethod() {
+	return self::$method;
+  }
+
+  /**
+   * Sets the superglobal(s) we should use when checking.
+   *
+   * @param  integer  $method
+   */
+  public static function setMethod($method) {
+	self::$method = $method;
+  }
+
+  /**
+   * Checks whether a form field is empty.
+   *
+   * @param  string          $field   The field to check.
+   * @param  integer | null  $method  The method to check for.
    *
    * @return  boolean
+   *
+   * @throws  InvalidArgumentException
    */
-  public static function isEmpty($field) {
-	if (!isset($_POST[$field])) return false;
-	return trim($_POST[$field]) === '';
+  public static function isEmpty($field, $method = null) {
+	if (is_null($method)) $method = self::$method;
+	switch ($method) {
+	  case self::METHOD_POST:
+		if (!isset($_POST[$field])) return false;
+		return trim($_POST[$field]) === '';
+	  case self::METHOD_GET:
+		if (!isset($_GET[$field])) return false;
+		return trim($_GET[$field]) === '';
+	  default:
+		throw new InvalidArgumentException('Invalid method.');
+	}
   }
 
 }
