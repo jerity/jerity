ebb7eb169e90e709ff6a350fa7c97dc36e732b9f
Nick Pope <nick@nickpope.me.uk>
2009-08-13 14:34:37 +0100

    Added initial tests for JerityCore\Tag.
    Includes simple tests for some of the basic tags, e.g. BASE, BR, HR, WBR
    
    Includes tests for both HTML 4.01 and XHTML 1.0
    
    Checks the result of new content masking methods.
    
    Added new Jerity Core Test Suite.

--- tests/tests/AllTests.php
+++ tests/tests/AllTests.php
@@ -1,11 +1,13 @@
 <?php
 require_once(dirname(dirname(__FILE__)).'/setUp.php');
+require_once(dirname(__FILE__).'/core/AllTests.php');
 require_once(dirname(__FILE__).'/template/AllTests.php');
 
 class Jerity_AllTests {
   public static function suite() {
     $suite = new PHPUnit_Framework_TestSuite();
 
+    $suite->addTestSuite(Jerity_Core_AllTests::suite());
     $suite->addTestSuite(Jerity_Template_AllTests::suite());
 
     return $suite;
---
+++ tests/tests/core/AllTests.php
@@ -0,0 +1,16 @@
+<?php
+require_once(dirname(dirname(dirname(__FILE__))).'/setUp.php');
+require_once('TagTestHTML401.php');
+require_once('TagTestXHTML10.php');
+
+class Jerity_Core_AllTests {
+  public static function suite() {
+    $suite = new PHPUnit_Framework_TestSuite();
+    $suite->setName('jerity-core');
+
+    $suite->addTestSuite('TagTestHTML401');
+    $suite->addTestSuite('TagTestXHTML10');
+
+    return $suite;
+  }
+}
