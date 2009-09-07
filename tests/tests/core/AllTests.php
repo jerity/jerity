6e22acc9c7d9c41f4dbc101970f5fbe6c3e751c8
Dave Ingram <dave@dmi.me.uk>
2009-09-07 02:15:11 +0100

    Added Core\ArrayUtil tests.

--- tests/tests/core/AllTests.php
+++ tests/tests/core/AllTests.php
@@ -1,5 +1,6 @@
 <?php
 require_once(dirname(dirname(dirname(__FILE__))).'/setUp.php');
+require_once('ArrayUtilTest.php');
 require_once('TagTestHTML401.php');
 require_once('TagTestXHTML10.php');
 
@@ -8,6 +9,7 @@ class Jerity_Core_AllTests {
     $suite = new PHPUnit_Framework_TestSuite();
     $suite->setName('jerity-core');
 
+    $suite->addTestSuite('ArrayUtilTest');
     $suite->addTestSuite('TagTestHTML401');
     $suite->addTestSuite('TagTestXHTML10');
 
