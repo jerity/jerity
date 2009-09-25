7c8beee0b1b704077e1b06d95c82bf2b6faec75c
Nick Pope <nick@nickpope.me.uk>
2009-09-25 17:36:44 +0100

    Tidied up comment spacing.

--- lib/core/exceptions/ImmutableObjectException.php
+++ lib/core/exceptions/ImmutableObjectException.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * @package  JerityCore
- * @author  Nick Pope <nick@nickpope.me.uk>
+ * @package    JerityCore
+ * @author     Nick Pope <nick@nickpope.me.uk>
  * @copyright  Copyright (c) 2009 Nick Pope
  */
 
 /**
  * Exception to throw if object has been deemed immutable.
  *
- * @package  JerityCore
- * @author  Nick Pope <nick@nickpope.me.uk>
+ * @package    JerityCore
+ * @author     Nick Pope <nick@nickpope.me.uk>
  * @copyright  Copyright (c) 2009 Nick Pope
  */
 class ImmutableObjectException extends Exception {
