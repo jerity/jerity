05829874533078cc6f161a95613a664c7b8e9042
Nick Pope <nick@nickpope.me.uk>
2009-09-25 17:37:38 +0100

    Added super exception and redirector exception.
    Introduces a new super exception for the Jerity Framework.  Any
    exception that is specific to Jerity should extend this.

---
+++ lib/core/exceptions/RedirectorException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package    JerityCore
+ * @author     Nick Pope <nick@nickpope.me.uk>
+ * @copyright  Copyright (c) 2009 Nick Pope
+ */
+
+/**
+ * Super-exception for classes in the Jerity Framework.
+ *
+ * @package    JerityCore
+ * @author     Nick Pope <nick@nickpope.me.uk>
+ * @copyright  Copyright (c) 2009 Nick Pope
+ */
+class RedirectorException extends JerityException {
+}
