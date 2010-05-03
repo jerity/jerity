d6b625d992855d532ccead1c2033ea79e62312f8
Nick Pope <nick@nickpope.me.uk>
2010-05-03 18:45:31 +0800

    Updated License: CC-BSD

--- tests/tests/core/AllTests.php
+++ tests/tests/core/AllTests.php
@@ -1,4 +1,13 @@
 <?php
+##############################################################################
+# Copyright © 2010 David Ingram, Nicholas Pope
+#
+# This work is licenced under the Creative Commons BSD License License. To
+# view a copy of this licence, visit http://creativecommons.org/licenses/BSD/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
+# San Francisco, California 94105, USA.
+##############################################################################
+
 require_once(dirname(dirname(dirname(__FILE__))).'/setUp.php');
 require_once('ArrayUtilTest.php');
 require_once('InflectorTest.php');
