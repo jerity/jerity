06e58c101b2053cd9e7cee3c694cdc641514476a
Nick Pope <nick@nickpope.me.uk>
2010-05-03 18:45:31 +0800

    Updated License: CC-BSD

--- tests/data/templates/vardump.tpl.php
+++ tests/data/templates/vardump.tpl.php
@@ -1,2 +1,11 @@
 <?php
+##############################################################################
+# Copyright © 2010 David Ingram, Nicholas Pope
+#
+# This work is licenced under the Creative Commons BSD License License. To
+# view a copy of this licence, visit http://creativecommons.org/licenses/BSD/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
+# San Francisco, California 94105, USA.
+##############################################################################
+
 var_dump(array_diff_key(get_defined_vars(), array('this'=>1)));
