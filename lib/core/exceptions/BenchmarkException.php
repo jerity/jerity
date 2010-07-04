d876345514bfbeeeb828291dea8ae5a05c30d6d2
Nick Pope <nick@nickpope.me.uk>
2010-07-05 00:02:49 +0100

    Added a class for benchmarking.

---
+++ lib/core/exceptions/BenchmarkException.php
@@ -0,0 +1,25 @@
+<?php
+##############################################################################
+# Copyright © 2010 David Ingram, Nicholas Pope
+#
+# This work is licenced under the Creative Commons BSD License License. To
+# view a copy of this licence, visit http://creativecommons.org/licenses/BSD/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
+# San Francisco, California 94105, USA.
+##############################################################################
+
+/**
+ * @package    jerity.core
+ * @author     Nick Pope <nick@nickpope.me.uk>
+ * @copyright  Copyright (c) 2010 Nick Pope
+ */
+
+/**
+ * Benchmarking exception class.
+ *
+ * @package    jerity.core
+ * @author     Nick Pope <nick@nickpope.me.uk>
+ * @copyright  Copyright (c) 2010 Nick Pope
+ */
+class BenchmarkException extends JerityException {
+}
