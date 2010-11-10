4a608eb8a75843d87e9bbd38beecb78130debda3
Nick Pope <nick@nickpope.me.uk>
2010-11-10 01:16:08 +0000

    Set minimum PHP version to 5.3

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -15,9 +15,15 @@ if (defined('JERITY_ROOT_PATH')) {
 
 # Set some constants:
 define('JERITY_ROOT_PATH'  , rtrim(dirname(__FILE__), '/'));
+define('JERITY_PHP_VERSION', '5.3');
+
+# Check PHP version:
+if (version_compare(PHP_VERSION, JERITY_PHP_VERSION, '<')) {
+  trigger_error('Jerity requires PHP '.JERITY_PHP_VERSION.' or later.', E_USER_ERROR);
+}
 
 # Enable full error reporting (including strict standards and deprecated):
-$_er_ = error_reporting(E_ALL | E_STRICT);
+$_er_ = error_reporting(E_ALL | E_STRICT | E_DEPRECATED);
 
 # Include the core utility class:
 require_once(JERITY_ROOT_PATH.'/Jerity.class.php');
