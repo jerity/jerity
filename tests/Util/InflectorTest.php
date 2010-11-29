5fb04b3db8a56914e54fe0f6ec30ff5e34a7ccf0
Nick Pope <nick@nickpope.me.uk>
2010-11-29 23:35:42 +0000

    Fixes to inflector tests.
    - Split out word lists to words, code, and abbreviations.
    - Add support for multiple singular/plural forms.
    - Remove reverse tests as can no longer guarantee the original form.
    - Update word lists to improve accuracy (using wiktionary).
    - Update rules to work correctly for new word lists.
    - Still need to fix some of the words in the main word list.

--- tests/Util/InflectorTest.php
+++ tests/Util/InflectorTest.php
@@ -22,122 +22,109 @@ use \Jerity\Util\Inflector;
 class InflectorTest extends PHPUnit_Framework_TestCase {
 
   /**
+   * Read in the word test data.
    *
+   * @return  array  The normal word test data.
    */
-  public static function getWordPairs() {
-    static $pairs = array();
-    if (count($pairs)) {
-      return $pairs;
-    }
-
-    $fp = fopen(__DIR__.'/singular-plural-wordlist.txt', 'r');
-    while (!feof($fp) && $data = fscanf($fp, '%s %s %d')) {
-      $pairs[] = $data;
+  protected static function getInflectorWordData() {
+    static $data = array();
+    if (count($data)) return $data;
+    $fp = fopen(__DIR__.'/../_data/inflector-word.dat', 'r');
+    while (!feof($fp) && $line = fscanf($fp, '%s %s')) {
+      # Skip commented lines:
+      if ($line[0][0] == '#') continue;
+      # Split multiple singular/plural forms:
+      $line[0] = explode('|', $line[0]);
+      $line[1] = explode('|', $line[1]);
+      $data[] = $line;
     }
     fclose($fp);
-
-    return $pairs;
+    return $data;
   }
 
   /**
+   * The normal word test data.
    *
+   * @return  array  The normal word test data.
    */
-  public static function wordPairProvider() {
-    return self::getWordPairs();
+  public static function inflectorWordProvider() {
+    return self::getInflectorWordData();
   }
 
+#  /**
+#   *
+#   */
+#  public function fixLetterCaseProvider() {
+#    return array(
+#      array('WORD',     'WORDS',    true),
+#      array('word',     'words',    true),
+#      array('Word',     'Words',    true),
+#      array('WoRd',     'WoRds',    true),
+#      array('VORTEX',   'VORTICES', true),
+#      array('vortex',   'vortices', true),
+#      array('Vortex',   'Vortices', true),
+#      array('VoRtEx',   'VoRtices', true),
+#      array('WORDS',    'WORD',     false),
+#      array('words',    'word',     false),
+#      array('Words',    'Word',     false),
+#      array('WoRds',    'WoRd',     false),
+#      array('VORTICES', 'VORTEX',   false),
+#      array('vortices', 'vortex',   false),
+#      array('Vortices', 'Vortex',   false),
+#      array('VoRtIcEs', 'VoRtex',   false),
+#    );
+#  }
+
   /**
+   * Test whether the inflector produces correct singular forms.
    *
-   */
-  public static function singularWordProvider() {
-    return array_map(function ($a) {
-      return array($a[0], $a[2]);
-    }, self::getWordPairs());
-  }
-
-  /**
+   * We check all plural forms yield the correct singular.
+   * Only the first (most common) singular form is acceptable.
    *
+   * @param  array  $singulars  The singular form(s) of the word.
+   * @param  array  $plurals    The plural form(s) of the word.
+   *
+   * @dataProvider  inflectorWordProvider()
    */
-  public static function pluralWordProvider() {
-    return array_map(function ($a) {
-      return array($a[1], $a[2]);
-    }, self::getWordPairs());
-  }
-
-  /**
-   * @dataProvider  wordPairProvider()
-   */
-  public function testSingularize($singular, $plural, $fix_case) {
-    $our_singular = Inflector::singularize($plural, true, $fix_case);
-    $this->assertSame($singular, $our_singular, 'Singular form of "'.$plural.'" differs');
-  }
-
-  /**
-   * @dataProvider  wordPairProvider()
-   */
-  public function testPluralize($singular, $plural, $fix_case) {
-    $our_plural = Inflector::pluralize($singular, true, $fix_case);
-    $this->assertSame($plural, $our_plural, 'Plural form of "'.$singular.'" differs');
-  }
-
-  /**
-   * @dataProvider  pluralWordProvider()
-   * @depends       testPluralize()
-   * @depends       testSingularize()
-   */
-  public function testSingularReverses($plural, $fix_case) {
-    $replural = Inflector::pluralize(Inflector::singularize($plural, true, $fix_case), true, $fix_case);
-    $this->assertSame($plural, $replural, 'Plural and replural form of "'.$plural.'" differ');
-  }
-
-  /**
-   * @dataProvider  singularWordProvider()
-   * @depends       testPluralize()
-   * @depends       testSingularize()
-   */
-  public function testPluralReverses($singular, $fix_case) {
-    $resingular = Inflector::singularize(Inflector::pluralize($singular, true, $fix_case), true, $fix_case);
-    $this->assertSame($singular, $resingular, 'Singular and resingular form of "'.$singular.'" differ');
-  }
-
-  /**
-   * @dataProvider  fixLetterCaseProvider()
-   * @depends       testPluralize()
-   * @depends       testSingularize()
-   */
-  public function testFixLetterCase($a, $b, $pluralise) {
-    # We disable simplification for this test to ensure that we do not split
-    # as camel case when testing with silly capitalisation.
-    if ($pluralise) {
-      $this->assertSame($b, Inflector::pluralize($a, false));
-    } else {
-      $this->assertSame($b, Inflector::singularize($a, false));
+  public function testSingularize($singulars, $plurals) {
+    foreach ($plurals as $plural) {
+      $output = Inflector::singularize($plural);
+      $this->assertSame($singulars[0], $output, "Unexpected singular form of '{$plural}'.");
     }
   }
 
   /**
+   * Test whether the inflector produces correct plural forms.
    *
+   * We check all singular forms yield the correct plural.
+   * Only the first (most common) plural form is acceptable.
+   *
+   * @param  array  $singulars  The singular form(s) of the word.
+   * @param  array  $plurals    The plural form(s) of the word.
+   *
+   * @dataProvider  inflectorWordProvider()
    */
-  public function fixLetterCaseProvider() {
-    return array(
-      array('WORD', 'WORDS', true),
-      array('word', 'words', true),
-      array('Word', 'Words', true),
-      array('WoRd', 'WoRds', true),
-      array('VORTEX', 'VORTICES', true),
-      array('vortex', 'vortices', true),
-      array('Vortex', 'Vortices', true),
-      array('VoRtEx', 'VoRtices', true),
-      array('WORDS', 'WORD', false),
-      array('words', 'word', false),
-      array('Words', 'Word', false),
-      array('WoRds', 'WoRd', false),
-      array('VORTICES', 'VORTEX', false),
-      array('vortices', 'vortex', false),
-      array('Vortices', 'Vortex', false),
-      array('VoRtIcEs', 'VoRtex', false),
-    );
+  public function testPluralize($singulars, $plurals) {
+    foreach ($singulars as $singular) {
+      $output = Inflector::pluralize($singular);
+      $this->assertSame($plurals[0], $output, "Unexpected plural form of '{$singular}'.");
+    }
   }
+
+#  /**
+#   * @dataProvider  fixLetterCaseProvider()
+#   * @depends       testPluralize()
+#   * @depends       testSingularize()
+#   */
+#  public function testFixLetterCase($a, $b, $pluralise) {
+#    # We disable simplification for this test to ensure that we do not split
+#    # as camel case when testing with silly capitalisation.
+#    if ($pluralise) {
+#      $this->assertSame($b, Inflector::pluralize($a, false));
+#    } else {
+#      $this->assertSame($b, Inflector::singularize($a, false));
+#    }
+#  }
 
 }
 
